417c2b10ac62f39e0d372779c7b88e9233658e9d
cekonsb <806138244@qq.com>
2020-07-04 16:16:18 +0800

    add first

---
+++ strategy/context.cpp
@@ -0,0 +1,21 @@
+//context.cpp
+#include "context.h"
+#include "strategy.h"
+#include <iostream>
+using namespace std;
+
+Context::Context(Strategy*stg)
+{
+    _stg = stg;
+}
+
+Context::~Context()
+{
+    if(!_stg)
+        delete _stg;
+}
+
+void Context::DoAction()
+{
+    _stg->AlgrithmInterface();
+}
